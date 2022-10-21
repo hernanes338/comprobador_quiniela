64b29e0619b17d7f2fed5fed7b3d132c620d08a0
hernanes338 <auherlpe@gmail.com>
2022-10-21 21:34:12 +0200

    eliminacion ficheros inutiles

--- players_bets.py
+++
@@ -1,6 +0,0 @@
-list_pronosticos = []
-
-list_pronosticos.append({'jugador': 'Dani' , 'pronostico':'112X211X221X11M1'})
-list_pronosticos.append({'jugador': 'Aure' , 'pronostico':'X12121112X21X1M1'})
-list_pronosticos.append({'jugador': 'Paco' , 'pronostico':'11X121X1XXX1X1M1'})
-list_pronosticos.append({'jugador': 'Omar' , 'pronostico':'X1X212X1X121X1M1'})
