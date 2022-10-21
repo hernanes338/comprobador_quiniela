dc5da9ca5c5dce0488e1a3d221edd9b2612199a3
hernanes338 <auherlpe@gmail.com>
2022-10-21 21:35:13 +0200

    import datetime comentado

--- results_history.py
+++ results_history.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import date, timedelta
+# from datetime import date, timedelta
 
 inicio_temporada = '20220814' # jornada 1 de la temporada 2022-2023
 ultima_jornada_completada = '20991231'
