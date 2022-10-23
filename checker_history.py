485d1b2002590744fe1242c760621ae97ee8e20d
hernanes338 <auherlpe@gmail.com>
2022-10-23 21:26:46 +0200

    script comprobador historico aciertos

--- checker_history.py
+++ checker_history.py
@@ -1,37 +1,48 @@
-# import results_history, bets_history
+import results_history, bets_history
 
-# list_resultados = results_history.list_resultados
+list_resultados = results_history.list_resultados
 
-# list_pronosticos = bets_history.list_pronosticos
+list_pronosticos = bets_history.list_pronosticos
 
+jornadas_con_pronosticos = []
 
-# resultado = list_resultados[0 - 1]['combinacion']
+for i in range(len(list_pronosticos)):
+    jornadas_con_pronosticos.append(list_pronosticos[i]['jornada'])
 
-# signos = resultado[:-2]
+list_aciertos = []
 
-# pleno15 = resultado[-2:]
+for i in range(len(list_pronosticos)):
+    jornada = list_pronosticos[i]['jornada']
+    resultado = ''
+    signos = ''
+    pleno15 = ''
+    for x in range(len(list_resultados)):
+        if list_pronosticos[i]['jornada'] == list_resultados[x]['jornada']:
+            resultado = list_resultados[x]['combinacion']
+            signos = resultado[:-2]
+            pleno15 = resultado[-2:]
+            break
 
-# list_aciertos = []
+    for j in range(len(list_pronosticos[i]['pronosticos'])):
+        aciertos = 0
+        jugador = list_pronosticos[i]['pronosticos'][j]['jugador']
+        pronostico = list_pronosticos[i]['pronosticos'][j]['pronostico']
+        
+        for k in range(len(signos)):
+            
+            if pronostico[k] == signos[k]:
+                aciertos += 1
 
-# for i in range(len(list_pronosticos)):
-#     aciertos = 0
-#     pronostico = list_pronosticos[i]['pronostico']
+        # comprobacion pleno al 15 (012M)
+        if aciertos == 14 and pleno15 == pronostico[-2:]:
+            aciertos += 1
 
-#     #comprobacion signos (1X2)
-#     for j in range(len(signos)):
-#         if pronostico[j] == signos[j]:
-#             aciertos += 1
-    
-#     # comprobacion pleno al 15 (012M)
-#     if aciertos == 14 and pleno15 == pronostico[-2:]:
-#         aciertos += 1
+        if resultado != '': 
+            list_aciertos.append(
+                {'jornada': jornada,
+                'jugador': jugador,
+                'aciertos': aciertos,
+                'pronostico': pronostico})
 
-#     # list_aciertos.append(
-#     #     {'jornada': jornada_comprobacion,
-#     #     'jugador': players_bets.list_pronosticos[i]['jugador'],
-#     #     'aciertos': aciertos,
-#     #     'pronostico': players_bets.list_pronosticos[i]['pronostico']})
-
-# for i in range(len(list_aciertos)):
-#     print(list_aciertos[i])
-
+for i in range(len(list_aciertos)):
+    print(list_aciertos[i])
